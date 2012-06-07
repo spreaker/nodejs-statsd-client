2eb3419aeee4293ff167ac1a18b518745050ee5e
Marco Pracucci <marco@pracucci.com>
2012-06-07 08:48:03 +0200

    Skeleton

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "node-statsd-client",
+  "version": "1.0.0",
+  "description": "Statsd client for node.js",
+  "keywords": ["statsd", "client"],
+  "repository": "https://github.com/spreaker/nodejs-statsd-client.git",
+  "author": "Marco Pracucci <marco.pracucci@spreaker.com>",
+  "dependencies": {},
+  "devDependencies": {},
+  "main": "index",
+  "engines": { "node": ">= 0.6.0" }
+}
